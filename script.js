bc2e4f71388ba6d7876aadfb23d3187c4722d43c
MAC <mac@MURRII-MacBook-Pro.local>
2022-09-29 11:19:29 +0100

    Refactored functions

--- script.js
+++ script.js
@@ -1,108 +1,77 @@
-let d = new Date();
+document.addEventListener("DOMContentLoaded", () => {
 
-function currentHour() {
-    let hours = d.getHours();
-    if (hours > 12) {
-        return hours = hours - 12;
-    } else if (hours < 10) {
-        return `0${hours}`;
-    } else if (hours == 0) {
-        return 12;
-    } else {
-        return hours;
-    }
+    currentTime();
+    todaysDate();
+    setInterval(currentTime, 1000);
+    setInterval(todaysDate, 1000);
+
+})
+
+
+let date = new Date();
+
+const getCurrentHour = () => {
+    const hours = date.getHours();
+    return (hours > 12) ? hours - 12
+        : (hours < 10) ? `0${hours}`
+            : (hours == 0) ? 12
+                : hours;
 }
 
 
-let currentMinute = () => {
-    let minutes = d.getMinutes();
-    if (minutes < 10) {
-        return minutes = `0${minutes}`;
-    } else {
-        return minutes;
-    }
+const getCurrentMinute = () => {
+    const minutes = date.getMinutes();
+    return (minutes < 10) ? `0${minutes}` : minutes;
+
 }
 
+const getCurrentSecond = () => {
+    const seconds = date.getSeconds();
+    return (seconds < 10) ? `0${seconds}` : seconds;
 
-let currentSecond = () => {
-    let seconds = d.getSeconds();
-    if (seconds < 10) {
-        return seconds = `0${seconds}`
-    } else {
-        return seconds;
-    }
 }
 
+const amOrPm = () => (date.getHours() >= 12) ? "pm" : "am";
 
-let amOrPm = () => {
-    if (d.getHours() >= 12) {
-        return "pm";
-    } else if (d.getHours() == 0) {
-        return "am";
-    } else {
-        return "am";
-    }
+function currentTime() {
+    date = new Date();
+    document.getElementById('clock').innerHTML = `
+    ${getCurrentHour()}:${getCurrentMinute()}:${getCurrentSecond()}`;
+    document.getElementById("am-pm").innerText = `${amOrPm()}`
 }
-
-
-let currentTime = () => {
-    d = new Date();
-    return document.getElementById('clock').innerHTML = `
-    ${currentHour()}:${currentMinute()}:${currentSecond()}<span> ${amOrPm()}</span>`;
-}
-
-setInterval(currentTime, 1000);
-
-
 
 // ************************************************ //
-// Code to display day, date, month, year
-// Function to retrieve day-name
 
-let currentDay = () => {
+const currentDay = () => {
 
-    let dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
-    let day = dayNames[d.getDay()];
+    const day = dayNames[date.getDay()];
     return day;
 }
 
+const currentMonth = () => {
 
-// Function to change abbreviated month name to full month name
-
-let currentMonth = () => {
-
-    let monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let month = monthNames[d.getMonth()];
+    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const month = monthNames[date.getMonth()];
     return month;
 }
 
 
+const currentDate = date.getDate();
 
-// Function to append letters to date number
+const dateAppender = (currentDate) => {
 
-let currentDate = d.getDate();
-function dateAppender(currentDate) {
-
-    if (currentDate === 1 || currentDate === 21 || currentDate === 31) {
-        return `${currentDate}st`;
-    } else if (currentDate === 2 || currentDate === 22) {
-        return `${currentDate}nd`;
-    } else if (currentDate === 3 || currentDate === 23) {
-        return `${currentDate}rd`;
-    } else {
-        return `${currentDate}th`;
-    }
+    return (currentDate === 1 || currentDate === 21 || currentDate === 31) ?
+        `${currentDate}st`
+        : (currentDate === 2 || currentDate === 22) ? `${currentDate}nd`
+            : (currentDate === 3 || currentDate === 23) ? `${currentDate}rd`
+                : `${currentDate}th`;
 }
 
-// Function to return current year
-let currentYear = () => d.getFullYear();
+const currentYear = date.getFullYear();
 
-
-// Call all functions and inject them into HTML document
-
-let todaysDate = () => {
-    return document.getElementById("date").innerHTML = currentDay() + ", " + currentMonth() + " " + dateAppender(currentDate) + " " + currentYear();
+const todaysDate = () => {
+    return document.getElementById("date").innerHTML = currentDay() + ", " + currentMonth() + " " + dateAppender(currentDate) + " " + currentYear;
 }
 
-todaysDate();
